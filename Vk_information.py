85551a9a5547d2b91b5c2127faf69d39105a97c4
Polina <ustinovapolina1@gmail.com>
2021-12-08 16:54:19 +0100

    some changes in the code. Now bot can talk with you and give you candidateds

--- Vk_information.py
+++ Vk_information.py
@@ -1,7 +1,8 @@
 import requests
+import pprint
 
-token_for_vk = input('Token for vk information:')
-#token_for_vk = ''
+token_for_vk = '34f4f11807efbe8c1b82a0850fa70f137c556b7221acde76b2acbf4e7d90fc485f5bd4fdd4ccb496e8e95' #input('Token for vk information:')
+#token_for_vk = input('Token for infomation in vk: ')
 def GetInfoFromVk(user_id):
     """ get infomration from the vk about person."""
     url_vk = 'https://api.vk.com/method/account.getProfileInfo'
@@ -15,7 +16,12 @@ def GetInfoFromVk(user_id):
     rrr = res[
         'response'
     ]
-    return rrr
+    return rrr['city']['title'], rrr['sex'],
+
+
+#a,b= GetInfoFromVk(670276685)
+#print(a)
+
 
 def GetPhotosVkData(user_id, count=5):
     """This function would get you the profile picture of the person in best resolution."""
@@ -33,3 +39,93 @@ def GetPhotosVkData(user_id, count=5):
     return rrr
     for k in rrr:
         return (k['sizes'][-1]['url'])
+
+
+def ListOfCities(user_id):
+    """This function would get you the list of cities in the country that you chosen."""
+    city, sex = GetInfoFromVk(user_id)
+    url_vk = 'https://api.vk.com/method/database.getCities'
+    params = {
+        'access_token': token_for_vk,
+        'country_id': 1,
+        'region_id': '',
+        'q': '',
+        'need_all': '',
+        'count': 100,
+        'v': 5.131
+        }
+    res = requests.get(url_vk, params=params).json()
+    rrr = res['response']['items']
+    for i in rrr:
+        if i['title'] == f'{city}':
+            return i['id']
+        return rrr
+
+
+
+def sex_number(sex_num):
+    if sex_num == 1:
+        return 2
+    else:
+        return 1
+
+def age_preferences(numer_for_age):
+    if numer_for_age == 1:
+        return 18, 24
+    elif numer_for_age == 2:
+        return 25, 34
+    elif numer_for_age == 3:
+        return 35, 44
+    elif numer_for_age == 4:
+        return 45, 54
+    elif numer_for_age == 5:
+        return 55, 64
+    elif numer_for_age == 6:
+        return 65, 74
+    elif numer_for_age == 7:
+        return 75, 100
+
+
+
+
+def get_users_for_date(user_id, number_for_age):
+    """ get random users photos, names and ids by your criteria for you """
+    number = ListOfCities(user_id)
+    city_name, sex_num = GetInfoFromVk(user_id)
+    num_for_part_sex = sex_number(sex_num)
+    age_from, age_to = age_preferences(number_for_age)
+    url_vk = 'https://api.vk.com/method/users.search'
+    params = {
+        'access_token': token_for_vk,
+        'q': '',
+        'sort': 0,
+        'offset': '',
+        'count': 3,
+        'fields': 'photo',
+        'country': 1,
+        'city': number,
+        'hometown': '',
+        'sex': num_for_part_sex,
+        'status': 6,
+        'age_from': age_from,
+        'age_to': age_to,
+        'has_photo': 1,
+        'v': 5.131
+    }
+    res = requests.get(url_vk, params=params).json()
+    rrr = res['response']['items']
+    values_of_key = [a_dict['photo'] for a_dict in rrr]
+    name = [a_dict['first_name'] for a_dict in rrr]
+    id_of_person = [a_dict['id'] for a_dict in rrr]
+    return values_of_key, name, id_of_person
+
+#print(get_users_for_date(670276685, 1))
+
+
+import pathlib
+pathlib.Path().resolve()
+
+import os
+#path = '/Users/mitch/Desktop/python class/python 2/thesis python 2'
+print(os.path.abspath(os.getcwd()))
+#print(os.listdir(path))
--- main.py
+++ main.py
@@ -2,10 +2,11 @@
 from random import randrange
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-from Vk_information import GetInfoFromVk, GetPhotosVkData
+from Vk_information import get_users_for_date
 
-#token = ''
-token = input('Token for group: ')
+#token = input('Token for groupchat in vk: ')
+token = '315435a4bb1ff15688e8693288424d0d8378539467d65061a3e14dd2889fe56fee19286ba695bb3424a4c'
+
 
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)
@@ -13,10 +14,18 @@ longpoll = VkLongPoll(vk)
 user_id_fromWR = []
 
 def BotWriteMsg(user_id, message):
-    """THis is a bot that can help you to find love"""
+    """THis is a bot that can help you to find love give messages"""
     vk.method('messages.send', {
         'user_id': user_id,
         'message': message,
+        'random_id': randrange(10 ** 7),})
+
+def BotSendPhoto(user_id, message, url):
+    """THis is a bot that can help you to find love give photos"""
+    vk.method('messages.send', {
+        'user_id': user_id,
+        'message': message,
+        'attachment': url,
         'random_id': randrange(10 ** 7),})
 
 
@@ -28,10 +37,45 @@ for event in longpoll.listen():
 
             if request == "привет":
                 BotWriteMsg(event.user_id,
-                          f"Привет я бот для try try me. Я могу помочь вам найти пару :D")
-                user_id_fromWR.append(event.user_id)
-                print(GetInfoFromVk(user_id_fromWR))
-                print(GetPhotosVkData(user_id_fromWR))
+                          f"Привет я бот для try try me. Я могу помочь вам найти пару. Вам помочь? :D")
+                user_id_fromWR.append(event.text) #event.user_id,
+            elif request == "да":
+                BotWriteMsg(event.user_id, "Какой вай возвратной диапазон? \n 1. 18-24 лет"
+                                           "\n2. 25-34 лет"
+                                           "\n3. 35-44 лет"
+                                           "\n4. 45-54 лет"
+                                           "\n5. 55-64 лет"
+                                           "\n6. 65-74 лет "
+                                           "\n7. 75 лет и старше "
+                                           "\nПожалуйста выберите любой из этих категорий и напишите номер в чат. :-)")
+            elif request == '1':
+                photo, name, id_of_person = get_users_for_date(event.user_id, 1)
+                for n, i, p in zip(name, id_of_person, photo):
+                    BotSendPhoto(event.user_id, f"{n} https://vk.com/id{i} \n", p)
+            elif request == '2':
+                photo, name, id_of_person = get_users_for_date(event.user_id, 2)
+                for n, i, p in zip(name, id_of_person, photo):
+                    BotSendPhoto(event.user_id, f"{n} https://vk.com/id{i} \n", p)
+            elif request == '3':
+                photo, name, id_of_person = get_users_for_date(event.user_id, 3)
+                for n, i, p in zip(name, id_of_person, photo):
+                    BotSendPhoto(event.user_id, f"{n} https://vk.com/id{i} \n", p)
+            elif request == '4':
+                photo, name, id_of_person = get_users_for_date(event.user_id, 4)
+                for n, i, p in zip(name, id_of_person, photo):
+                    BotSendPhoto(event.user_id, f"{n} https://vk.com/id{i} \n", p)
+            elif request == '5':
+                photo, name, id_of_person = get_users_for_date(event.user_id, 5)
+                for n, i, p in zip(name, id_of_person, photo):
+                    BotSendPhoto(event.user_id, f"{n} https://vk.com/id{i} \n", p)
+            elif request == '6':
+                photo, name, id_of_person = get_users_for_date(event.user_id, 6)
+                for n, i, p in zip(name, id_of_person, photo):
+                    BotSendPhoto(event.user_id, f"{n} https://vk.com/id{i} \n", p)
+            elif request == '7':
+                photo, name, id_of_person = get_users_for_date(event.user_id, 7)
+                for n, i, p in zip(name, id_of_person, photo):
+                    BotSendPhoto(event.user_id, f"{n} https://vk.com/id{i} \n", p)
             elif request == "пока":
                 BotWriteMsg(event.user_id, "Пока((")
             else:
